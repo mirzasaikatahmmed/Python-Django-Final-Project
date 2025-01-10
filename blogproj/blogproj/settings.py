205a7657cafbfb8a1bc495853d13f239b4760346
Mirza Saikat Ahmmed <mirzasaikatahmmed@gmail.com>
2025-01-10 23:15:22 +0600

    Update

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*_@dvi^g6%!&dln=76=l1ok4=hl+$clzu&&^dz472z^!80r+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
